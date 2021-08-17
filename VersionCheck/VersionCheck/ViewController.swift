a91d8e00016509e791092bd93594067ba078cbbd
swieeft <swieeft89@gmail.com>
2021-08-17 16:32:04 +0900

    Migor, Minor, Patch 순서대로 비교하는 로직 추가

--- VersionCheck/VersionCheck/ViewController.swift
+++ VersionCheck/VersionCheck/ViewController.swift
@@ -23,23 +23,53 @@ class ViewController: UIViewController {
             return
         }
         
-        let server = serverText.split(separator: ".")
-        let app = appText.split(separator: ".")
+        let serverVersion = serverText.split(separator: ".")
+        let appVersion = appText.split(separator: ".")
         
-        var isUpdate = false
         
-        for i in (0..<3).reversed() {
-            guard let s = Int(server[i]), let a = Int(app[i]) else {
-                continue
-            }
-            
-            checkVersion(server: s, app: a, isUpdate: &isUpdate)
-        }
-        
-        resultLabel.text = "\(isUpdate)"
+        // Migor, Minor, Patch 순서대로 비교
+//        guard let serverMigor = Int(serverVersion[0]),
+//              let serverMinor = Int(serverVersion[1]),
+//              let serverPatch = Int(serverVersion[2]),
+//              let appMigor = Int(appVersion[0]),
+//              let appMinor = Int(appVersion[1]),
+//              let appPatch = Int(appVersion[2]) else {
+//            return
+//        }
+//
+//        if appMigor < serverMigor {
+//            resultLabel.text = "\(true)"
+//        } else if appMigor == serverMigor {
+//            if appMinor < serverMinor {
+//                resultLabel.text = "\(true)"
+//            } else if appMinor == serverMinor {
+//                if appPatch < serverPatch {
+//                    resultLabel.text = "\(true)"
+//                } else {
+//                    resultLabel.text = "\(false)"
+//                }
+//            } else {
+//                resultLabel.text = "\(false)"
+//            }
+//        } else {
+//            resultLabel.text = "\(false)"
+//        }
+
+        // Patch, Minor, Migor 순서대로 비교
+//        var isUpdate = false
+//
+//        for i in (0..<3).reversed() {
+//            guard let s = Int(serverVersion[i]), let a = Int(appVersion[i]) else {
+//                continue
+//            }
+//
+//            checkVersion(server: s, app: a, isUpdate: &isUpdate)
+//        }
+//
+//        resultLabel.text = "\(isUpdate)"
     }
     
-    func checkVersion(server: Int, app: Int, isUpdate: inout Bool) {// -> Bool {
+    func checkVersion(server: Int, app: Int, isUpdate: inout Bool) {
         if server > app {
             isUpdate = true
         } else if server < app {
